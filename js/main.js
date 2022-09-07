3d6b171349e43e25e41e013a7dfa0368034983fb
UrijHoruzij <63039080+UrijHoruzij@users.noreply.github.com>
2022-09-07 10:57:16 +0000

    Deploying to main from  @ a25463f90c96867a2f52a63d9bdcde5854b436a2 🚀

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+	var flag = false;
+	var hamburger = document.querySelector('.sidebar__hamburger');
+	var navigation = document.querySelector('.sidebar__navigation');
+	console.log(navigation);
+	hamburger.addEventListener('click', function () {
+		if (flag) {
+			hamburger.classList.remove('sidebar__hamburger--activ');
+			navigation.classList.remove('sidebar__navigation--activ');
+		} else {
+			hamburger.classList.add('sidebar__hamburger--activ');
+			navigation.classList.add('sidebar__navigation--activ');
+		}
+		flag = !flag;
+	});
+});
